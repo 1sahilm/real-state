0b273307f56247a072f0a936b9a74646e3784164
sahilsimply <102518542+sahilsimply@users.noreply.github.com>
2025-07-02 16:09:35 +0530

    updated code

--- components/Blog/WeDesign/page.tsx
+++ components/Blog/WeDesign/page.tsx
@@ -10,7 +10,7 @@ const WeDesign = ()=>{
 A curated space where luxury living, design inspiration, and progress updates come together.</h2>
                 <div className={Style.pere}>
     <p>At Vvie Villas, we believe a home is more than just a space — it’s a lifestyle rooted in elegance, nature, and thoughtful detail. Through this journal, we invite you to explore our world: from architectural vision and investment insights to handpicked destinations and the evolving journey of bringing each villa to life.</p>
-    <p>Whether you're planning your next investment, dreaming of a custom-built escape, or following the progress of your future home, this is where you'll find the stories, updates, and inspirations that define the Vvie way of life.</p>
+    <p>Whether you&apos;re planning your next investment, dreaming of a custom-built escape, or following the progress of your future home, this is where you&apos;ll find the stories, updates, and inspirations that define the Vvie way of life.</p>
     <p>Stay inspired. Stay informed. Live Vvie.</p>
 </div>
 
